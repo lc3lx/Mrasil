2fae21e22ee6450c70a2b192f35abb507a74bef2
lc3lx <140909706+lc3lx@users.noreply.github.com>
2025-08-12 09:07:48 +0300

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://backend-marasil.onrender.com/api"; 
-// export const API_BASE_URL = "http://localhost:4000/api"; 
+export const API_BASE_URL = "https://www.marasil.site/api";
+// export const API_BASE_URL = "http://localhost:4000/api";
