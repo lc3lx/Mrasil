572a70538e7a6212659417d62dca376072c27d54
lc3lx <140909706+lc3lx@users.noreply.github.com>
2025-08-20 14:28:14 +0300

    Update _app.tsx

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,12 +4,16 @@ import { store } from '../store/store';
 import { AuthProvider } from '../providers/AuthProvider';
 import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <AuthProvider>
-        <Component {...pageProps} />
+        <Head>
+  <link rel="stylesheet" href="https://unpkg.com/moyasar-payment-form@2.0.16/dist/moyasar.css" />
+</Head>
+<Component {...pageProps} />
         <Toaster position="top-right" />
       </AuthProvider>
     </Provider>
