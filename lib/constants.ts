aced485b945bdabe8897bffe47420235a143314c
lc3lx <lc3lx@users.noreply.github.com>
2025-09-25 13:54:11 +0300

    edit the url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,2 @@
-
-// export const API_BASE_URL = "https://www.marasil.site/api";
-export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "https://www.marasil.site/api";
+//export const API_BASE_URL = "http://localhost:8000/api";
